c710b332b88baec34c5644d3e91477f3d1e9b420
mody3mr <99924090+mody3mr@users.noreply.github.com>
2026-09-13 15:10:06 +0300

    Add Firebase configuration for dashboard

---
+++ dashboard/app-config.js
@@ -0,0 +1,13 @@
+const dashboardFirebaseConfig = {
+    apiKey: "AIzaSyBNwQKnuIomQGm41bsGclI9ihY3SiK78eU",
+    authDomain: "saybr-masr.firebaseapp.com",
+    databaseURL: "https://saybr-masr-default-rtdb.firebaseio.com",
+    projectId: "saybr-masr",
+    storageBucket: "saybr-masr.firebasestorage.app",
+    messagingSenderId: "650974582933",
+    appId: "1:650974582933:web:01ad2aaf2e89c9b668639c"
+};
+
+const dashboardFirebaseApp = firebase.initializeApp(dashboardFirebaseConfig, 'saybrMasrDashboard');
+const dashboardDb = firebase.database(dashboardFirebaseApp);
+const dashboardAuth = firebase.auth(dashboardFirebaseApp);
